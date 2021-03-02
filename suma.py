98f15803b6d0f97142384e6df727f65caa1e232a
Walter0210 <walter.ebalderrama@gmail.com>
2021-03-02 20:11:21 -0300

    La suma es de int, pero ahora hay menú de opciones

--- suma.py
+++ suma.py
@@ -1,6 +1,33 @@
+
+while True:
+    print('1. Sumar')
+    print('2. Restar')
+    print('3. Ver Log')
+    print('4. Salir')
+    op = int(input('\tSeleccione una opción: '))
+
+    if op == 1:
+        try:
+            a = int(input('\nIngrese un numero: '))
+            b = int(input('Ingrese otro numero: '))
+
+            res = a + b
+            print(str(a) + ' + ' + str(b) + ' = ' + str(res) + '\n')
+        except:
+            print('Ingrese solo valores numéricos')
+
+    elif op == 2:
+        #restar
+        print('restar')
+        break
+    elif op == 3:
+        #logger
+        print('log')
+        break
+
 try:
-    a = float(input('Ingrese un numero: '))
-    b = float(input('Ingrese un numero: '))
+    a = int(input('Ingrese un numero: '))
+    b = int(input('Ingrese otro numero: '))
 
     res = a + b
     print(res)
