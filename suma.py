5caa8bb8c79941d07209fd14c5d7a69af84cd861
Walter0210 <walter.ebalderrama@gmail.com>
2021-03-02 21:06:50 -0300

    la misma funcion puede sumar o restar

--- suma.py
+++ suma.py
@@ -1,5 +1,19 @@
+def operacion(flag):
+    try:
+        a = float(input('\nIngrese un numero: '))
+        b = float(input('Ingrese otro numero: '))
+        if flag:
+            res = a + b
+            print('\n\t' + str(a) + ' + ' + str(b) + ' = ' + str(res) + '\n')
+        else:
+            res = a - b
+            print('\n\t' + str(a) + ' - ' + str(b) + ' = ' + str(res) + '\n')
+    except:
+        print('\n\tIngrese solo valores numéricos!\n\n')
+
 
 while True:
+    
     print('1. Sumar')
     print('2. Restar')
     print('3. Ver Log')
@@ -7,19 +21,11 @@ while True:
     op = int(input('\tSeleccione una opción: '))
 
     if op == 1:
-        try:
-            a = int(input('\nIngrese un numero: '))
-            b = int(input('Ingrese otro numero: '))
-
-            res = a + b
-            print(str(a) + ' + ' + str(b) + ' = ' + str(res) + '\n')
-        except:
-            print('Ingrese solo valores numéricos')
+        operacion(True)
 
     elif op == 2:
-        #restar
-        print('restar')
-        break
+        operacion(False)
+
     elif op == 3:
         #logger
         print('log')
