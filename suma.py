8fbbbc88f65280ee3344393bfe95e4077a0e2200
Walter0210 <walter.ebalderrama@gmail.com>
2021-03-03 08:45:51 -0300

    operacion retorna un string y se logea en el ciclo

--- suma.py
+++ suma.py
@@ -1,19 +1,24 @@
+
 def operacion(flag):
+    strRes = False
     try:
         a = float(input('\nIngrese un numero: '))
         b = float(input('Ingrese otro numero: '))
         if flag:
             res = a + b
-            print('\n\t' + str(a) + ' + ' + str(b) + ' = ' + str(res) + '\n')
+            strRes = ('\n\t' + str(a) + ' + ' + str(b) + ' = ' + str(res) + '\n')
+            
         else:
             res = a - b
-            print('\n\t' + str(a) + ' - ' + str(b) + ' = ' + str(res) + '\n')
+            strRes = ('\n\t' + str(a) + ' - ' + str(b) + ' = ' + str(res) + '\n')
     except:
         print('\n\tIngrese solo valores numéricos!\n\n')
+    finally:
+        return strRes
 
-
+log = ''
 while True:
-    
+
     print('1. Sumar')
     print('2. Restar')
     print('3. Ver Log')
@@ -21,15 +26,19 @@ while True:
     op = int(input('\tSeleccione una opción: '))
 
     if op == 1:
-        operacion(True)
-
+        sum = operacion(True)
+        print(sum)
+        log += sum
+        
+        
     elif op == 2:
-        operacion(False)
+        res = operacion(False)
+        print(res)
+        log += res
 
-    elif op == 3:
-        #logger
-        print('log')
-        break
+    elif op == 3:       
+        print(log)
+             
 
 try:
     a = int(input('Ingrese un numero: '))
