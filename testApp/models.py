34fc939d4b5ece0c3533604fd33f3d8ef0455ad8
Walter0210 <walter.ebalderrama@gmail.com>
2021-03-03 19:40:31 -0300

    modelo basico con capos

--- testApp/models.py
+++ testApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    text = models.TextField()
+    fechaCreacion = models.DateTimeField(default=timezone.now)
+    fechaPubli = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.fechaPubli = timezone.now()
+        self.save
+
+    def __str__(self):
+        return self.titulo
