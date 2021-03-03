1ebc0b07be5c74e4ade19488a57015afce06615e
Walter0210 <walter.ebalderrama@gmail.com>
2021-03-03 10:02:06 -0300

    el registro se vacia al iniciar el script

--- suma.py
+++ suma.py
@@ -2,49 +2,58 @@
 def operacion(flag):
     strRes = False
     try:
-        a = float(input('\nIngrese un numero: '))
+        a = float(input('Ingrese un numero: '))
         b = float(input('Ingrese otro numero: '))
         if flag:
             res = a + b
-            strRes = ('\n\t' + str(a) + ' + ' + str(b) + ' = ' + str(res) + '\n')
+            strRes = ('\n\t' + str(a) + ' + ' + str(b) + ' = ' + str(res))
             
         else:
             res = a - b
-            strRes = ('\n\t' + str(a) + ' - ' + str(b) + ' = ' + str(res) + '\n')
+            strRes = ('\n\t' + str(a) + ' - ' + str(b) + ' = ' + str(res))
     except:
         print('\n\tIngrese solo valores numéricos!\n\n')
     finally:
         return strRes
 
-log = ''
+log = open("logger.txt", "w")
 while True:
-
+    
+    print('\n.::Menú principal::.')
     print('1. Sumar')
     print('2. Restar')
     print('3. Ver Log')
     print('4. Salir')
-    op = int(input('\tSeleccione una opción: '))
+    try:
+        op = int(input('\tSeleccione una opción: '))
 
-    if op == 1:
-        sum = operacion(True)
-        print(sum)
-        log += sum
-        
-        
-    elif op == 2:
-        res = operacion(False)
-        print(res)
-        log += res
+        if op == 1:
+            print("\t\nSuma de dos números:")
+            sum = operacion(True)
+            if sum != False:
+                log = open("logger.txt", "a")
+                print(sum)
+                log.write(sum)
+                log.close()
+            
+            
+        elif op == 2:
+            print("\t\nResta de dos números:")
+            rest = operacion(False)
+            if rest != False:
+                log = open("logger.txt", "a")
+                print(rest)
+                log.write(rest)
+                log.close()
 
-    elif op == 3:       
-        print(log)
-             
+        elif op == 3:    
+            print("\t\nRegistro de operaciones:")
+            log = open("logger.txt", "r")   
+            print(log.read())
+            log.close()
 
-try:
-    a = int(input('Ingrese un numero: '))
-    b = int(input('Ingrese otro numero: '))
-
-    res = a + b
-    print(res)
-except:
-    print('Ingrese solo valores numéricos')
+        elif op == 4:
+            print("Gracias! nos vemos!")
+            break
+    except:
+        print('Seleccione una opcion valida!')
