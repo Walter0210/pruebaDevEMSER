2be76e3c4c90ad0716bc1d8f80906dede85a79c2
Walter0210 <walter.ebalderrama@gmail.com>
2021-03-03 22:28:32 -0300

    alta de modelos

--- testApp/admin.py
+++ testApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Pagina1
+
+admin.site.register(Pagina1)
 
 # Register your models here.
--- testApp/models.py
+++ testApp/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
-class Post(models.Model):
+class Pagina1(models.Model):
     autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=200)
     text = models.TextField()
     fechaCreacion = models.DateTimeField(default=timezone.now)
     fechaPubli = models.DateTimeField(blank=True, null=True)
 
-    def publish(self):
+    def publicar(self):
         self.fechaPubli = timezone.now()
         self.save
 
