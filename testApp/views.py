a1ffe5674ecc91e4cefe91556cbf6bec270a3d77
Walter0210 <walter.ebalderrama@gmail.com>
2021-03-05 11:39:46 -0300

    modelos en htmls, ciclos y demas

--- testApp/views.py
+++ testApp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Model1
+from django.contrib.auth.models import User
 
 def post_list(request):
-    return render(request, 'testApp/post_list.html', {})
+    usuarios = User.objects.all()
+    modelos = Model1.objects.all()
+    return render(request, 'testApp/post_list.html', {'modelos' : modelos})
